657f54f0fc60a3da142f7389a70470222ab42583
aleyna-yildizli <aleynayildizli@gmail.com>
2024-01-10 15:18:50 +0300

    Header InfoSection Added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css';
+import { useLocation } from 'react-router-dom';
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+  const location = useLocation();
+  
+    if (location.pathname === '/pizza') {
+     show==false && setShow(true);
+    } else {
+     show==true && setShow(false);
+    }
+ 
+
 
   return (
     <div className='headerMain'>
        <img src="../../logo.svg" />
+       {show && (
+        <div className='headerMainInfo'>
+          <p className='headerMainInfoTitle'>Ana Sayfa &nbsp;-&nbsp;</p>
+          <p className='headerMainInfoDesc'>Sipariş Oluştur </p>
+       </div>
+       )}
     </div>
   );
 };
